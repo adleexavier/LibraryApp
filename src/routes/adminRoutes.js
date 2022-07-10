673a71594958c2c01bfa96b6804a3a4f676b9e13
Adlee Xavier <adleexavier@gmail.com>
2022-07-11 02:08:18 +0530

    delete operation done stuck with create operation

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -12,10 +12,10 @@ function route (nav){
         res.render("add_book",{
             nav,
             title: 'Library'
-        } )
+                 })
      
-     })
-    adminRouter.get("/gets",(req,res)=>{
+                   })
+    adminRouter.get("/create",(req,res)=>{
       var data = {      
         title  : req.query.title,
         author : req.query.author,
@@ -24,33 +24,42 @@ function route (nav){
       
       } // you need to pass these data from the form using (get method) 
       
-     var book = bookdata(data) // using the schema the data is been structure in the way the schema is designed
+      bookdata(data).save();            // using the schema the data is been structure in the way the schema is designed
                                // SAVING part will be done by this file ie adminRoutes.js
-      book.save();             // this is how you save it to the database
+                              // this is how you save it to the database
       res.redirect("/books");
-    });
+                 });
 
-    adminRouter.post("/pos", urlencodedParser,(req,res)=>{ 
-                var data = {      
-                  title  : req.body.title, // in the post method we are taking the data from html body <<<<---
-                  author : req.body.author,
-                  Genere : req.body.Genere,
-                  image: req.body.Image
-                
-                } // DETAILS OF THE FORM CANNOT BE SEEN AS QUERY PARAMETER
-                  // THIS IS WHY POST IS MORE SECURE THAN THE GET
-                
-                bookdata.remove({title:title},(err,data)=>{
-                            if(err){
-                              console.log(err);
-                                  }
-                          else{
-                              res.send(data);
-                              }
+    adminRouter.post("/delete", urlencodedParser,(req,res)=>{ 
+      var data = {      
+        title  : req.body.title, // in the post method we are taking the data from html body <<<<---
+        author : req.body.author,
+        Genere : req.body.Genere,
+        image: req.body.Image
+      
+      } // DETAILS OF THE FORM CANNOT BE SEEN AS QUERY PARAMETER
+        // THIS IS WHY POST IS MORE SECURE THAN THE GET
+      var book = bookdata(data);
+      var title = book.title;
+      bookdata.deleteOne({title,title}).then(()=>{
+        res.redirect("/books");
 
-                              });
+                       });
+     
+                   });
+    adminRouter.post("/update", urlencodedParser,(req,res)=>{
 
-                    });
+       var condition = {_id:req.body.id}
+       console.log(condition)
+       var request = req.body;
+       console.log(request);
+       bookdata.updateOne({condition,request}).then(()=>{
+          res.redirect("/books")
+                        });
+                   
+    
+    
+                   });
 
 
 
